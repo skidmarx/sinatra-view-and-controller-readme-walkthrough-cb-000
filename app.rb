606f428fe4a9c2d74f4383f9350eec67f296eddc
Michael Palmer <lawlux@gmail.com>
2017-10-11 16:20:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reverse' do
+    puts params
     erb :reverse
   end
 
   post '/reverse' do  
-    puts params  
+    
     erb :reversed
   end
 
